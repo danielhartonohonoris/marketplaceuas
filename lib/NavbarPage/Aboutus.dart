9ded2538943c3332555307795c8d4bf73b3f8ac0
valentinorichardo <valentinorichardololz@gmail.com>
2023-12-01 13:53:15 +0700

    mengubah tampilan about us

--- lib/NavbarPage/Aboutus.dart
+++ lib/NavbarPage/Aboutus.dart
@@ -13,76 +13,111 @@ class AboutUs extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            const SizedBox(
-              height: 20,
-            ),
-            Image.asset(
-              'image/account.png',
-              width: 200,
-              height: 200,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              'E-Commerce App',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+            const SizedBox(height: 20),
+            Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Colors.red.shade600,
+                  width: 3,
+                ),
+              ),
+              child: CircleAvatar(
+                radius: 100,
+                backgroundImage: AssetImage('image/account.png'),
               ),
             ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(
-              'KELOMPOK MARKETPLACE',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.red.shade400,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              'VISI & MISI ',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+            const SizedBox(height: 20),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 30),
+              child: Text(
+                'E-Commerce App',
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                ),
               ),
             ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(
-              'Memajukan sebuah karya marketplace yang kita pelajari untuk masuk kedalam dunia pekerjaan IT',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.red.shade400,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              'Contact Us',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+            Container(
+              margin: const EdgeInsets.only(bottom: 20),
+              child: Text(
+                'Exclusively Crafted by KELOMPOK MARKETPLACE',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.red.shade400,
+                ),
+                textAlign: TextAlign.center,
               ),
             ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(
-              'Email: marketplaceprojectuas@gmail.com \nPhone: +0822-1234-4321',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.red.shade400,
+            Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    color: Colors.red.shade600,
+                    width: 2,
+                  ),
+                ),
               ),
-              textAlign: TextAlign.center,
+              padding: const EdgeInsets.only(bottom: 20),
+              child: Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 10),
+                    child: Text(
+                      'Our Vision & Mission',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black87,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'To advance the realm of IT employment through our diligently crafted marketplace project.',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.red.shade400,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 20),
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    color: Colors.red.shade600,
+                    width: 2,
+                  ),
+                ),
+              ),
+              padding: const EdgeInsets.only(bottom: 20),
+              child: Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 10),
+                    child: Text(
+                      'Contact Us',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black87,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'Email: marketplaceprojectuas@gmail.com\nPhone: +0822-1234-4321',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.red.shade400,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
             ),
           ],
         ),
